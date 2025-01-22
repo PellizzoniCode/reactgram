aa0c09b25052aaf0385b92253cdb0801c347c7d2
Giovanni Pellizzoni <pellizzonicode@gmail.com>
2025-01-22 09:13:18 +0100

    initialize backend with user and photo models, routes, and validation middleware

---
+++ backend/middlewares/imageUpload.js
@@ -0,0 +1,28 @@
+const multer = require("multer");
+const path = require("path");
+
+const imageStorage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    let folder = "";
+    if (req.baseUrl.includes("users")) {
+      folder = "user";
+    } else if (req.baseUrl.includes("photos")) {
+      folder = "photos";
+    }
+
+    callback(null, `uploads/${folder}/`);
+  },
+  filename: (req, file, callback) => {
+    callback(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const imageUpload = multer({
+  storage: imageStorage,
+  fileFilter: (req, file, callback) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return callback(new Error("Por favor, envie apenas png, jpg ou jpeg"));
+    }
+    callback(null, true);
+  },
+});
---
+++ backend/routes/UserRoutes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+
+const validate = require("../middlewares/handleValidation");
+
+const {
+  register,
+  login,
+  getCurrentUser,
+  update,
+} = require("../controllers/UserController");
+const authGuard = require("../middlewares/authGuard");
+// Routes
+
+const {
+  userCreateValidation,
+  loginValidation,
+  userUpdateValidation,
+} = require("../middlewares/userValidations");
+
+router.post("/register", userCreateValidation(), validate, register);
+router.post("/login", loginValidation(), validate, login);
+router.get("/profile", authGuard, getCurrentUser);
+router.put(
+  "/",
+  authGuard,
+  userUpdateValidation(),
+  validate,
+  imageUpload.single("profileImage"),
+  update
+);
+
+module.exports = router;
